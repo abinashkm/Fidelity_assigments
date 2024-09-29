8e4d4c14ebf9d18f14e2fdb1543d7331d2b8b6ac
Abinash Mohanty <abinashkummar333@gmail.com>
2024-09-30 00:33:28 +0530

    assigment 5

---
+++ assigment5/src/signedByte.java
@@ -0,0 +1,15 @@
+
+import java.util.*;
+
+public class signedByte {
+    public static void main(String[] args){
+
+        Scanner sc= new Scanner(System.in);
+
+        System.out.println("Enter number : ");
+        byte num= sc.nextByte();
+        System.out.println("Value of Number : "+num);
+        num=127;
+        System.out.println("Largest value stored in a signed byte : "+num);
+    }
+}
